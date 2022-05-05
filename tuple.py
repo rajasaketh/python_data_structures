9010bc83afdc965864482214512058d49866c4c1
Raja Behata <raja.behata@citi.com>
2022-05-05 16:46:22 -0500

    initial version of the tuples

---
+++ tuple.py
@@ -0,0 +1,17 @@
+empty_tuple = ()
+print(empty_tuple)
+
+#Create tuple with one element
+single_tuple = ('one',)
+print(single_tuple)
+
+single_tuple2 = 'one',
+print(single_tuple2)
+
+# Caution -Single element in paranthesis omitting the comma would not get a tuple
+single_tuple3 = ('one')
+print(type(single_tuple3))
+
+#Create tuple with multiple elements
+multiple_tuple = ('one', 'two', 'three')
+print(multiple_tuple)
